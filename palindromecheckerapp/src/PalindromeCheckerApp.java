a10df3551d4bb77f7c74c07d505ea585a8db1da1
Manasviii20 <manasvijhade0209@gmail.com>
2026-02-24 15:29:12 +0530

    palindrome app updated with uc3

---
+++ palindromecheckerapp/src/PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+    static void main() { // Hardcoded string (String Literal)
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string to check if it is a palindrome:");
+        String originalString = scanner.nextLine();
+
+        String reversedString = "";
+
+        for (int i = originalString.length() - 1; i >= 0; i--) {
+            reversedString = reversedString + originalString.charAt(i);
+        }
+        if (originalString.equals(reversedString)) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        scanner.close();
+    }
+}
